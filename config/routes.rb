133c24fd386f81cce7f33844402afd0c706d96a8
awadh <awadh.hassan@student.moringaschool.com>
2022-12-07 11:53:02 +0300

    adddition of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :reviews
   resources :products, only: [:index, :show, :destroy, :create, :update]
 
@@ -6,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :create, :edit, :update, :show, :destroy]
+
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sesions#destroy'
+  get '/logout',to: 'sessions#destroy'
 end
