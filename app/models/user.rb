1201d0323f6473c5842319a8f2e69a42ca6bb133
awadh <awadh.hassan@student.moringaschool.com>
2022-12-07 13:06:11 +0300

    active records

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :products
+    has_many :reviews
+
+
+    validates :username, presence: true
+    validates :password, presence: true
+    
 end
