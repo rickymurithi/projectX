db8892412cf67428777ed5e9006a0ff9adc907b7
erickmuriithi <rickymurithii@gmail.com>
2022-12-16 00:48:24 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,102 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Product.create(name: "Toy", description: "Kids-toy", price: 1)
+
+
+# Product.create(name: "Toy", description: "Kids-toy", price: 1)
+
+products = Product.create([
+    {
+        image: "https://images.pexels.com/photos/760979/pexels-photo-760979.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "Trucks",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://images.pexels.com/photos/207923/pexels-photo-207923.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        title: "Dolls",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 171067,
+    },
+    {
+        image: "https://images.unsplash.com/photo-1599623560574-39d485900c95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        title: "Toys",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-720w,f_auto,q_auto:best/newscms/2021_44/1799142/517j5ignril-sl500--618437f0e28a6.jpg",
+        title: "Toy",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4i1UCtLzDid_Reu4ePWFC45m1VdPVWWaUhg&usqp=CAU",
+        title: "Puzzles",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://cdn.thewirecutter.com/wp-content/media/2021/11/kidsgiftsupdates-2048px-waterblocks.jpg",
+        title: "Shapes",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1604339959-51FiNQT6oRL.jpg?crop=1xw:1.00xh;center,top&resize=480:*",
+        title: "Lego",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://www.hellomagazine.com/imagenes/healthandbeauty/mother-and-baby/20210917121817/top-christmas-toys-for-kids-of-all-ages/0-601-247/hamleys-top-toys-2021-playmobil-city-action-z.jpg",
+        title: "Animated",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://media.wired.com/photos/59269b9acfe0d93c47430cee/master/w_1828,h_1371,c_limit/01-Modarri-cars-SOURCE-Modarri.jpg",
+        title: "RC cars",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://d1jqecz1iy566e.cloudfront.net/med4/fb426.jpg",
+        title: "Toys",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://www.shoprite.co.za/medias/10624393EA-checkers300Wx300H?context=bWFzdGVyfGltYWdlc3w5NDY0NnxpbWFnZS9wbmd8aW1hZ2VzL2hlYi9oOTAvOTg0NDU1MjY2MzA3MC5wbmd8MjA0ZDU4ZjQwZWNiODgwODYxODAzMmJjMzkwMjY3NDVmYTY3OWE0NGRiN2Y3MjhiY2I1YzNhNGQxODUwNDU0ZQ",
+        title: "Rides",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://food.fnr.sndimg.com/content/dam/images/food/products/2019/11/22/rx_dragons-love-tacos.jpeg.rend.hgtvcom.616.616.suffix/1574459089363.jpeg",
+        title: "Storys",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://i.pinimg.com/originals/b0/68/b7/b068b768634e3bd86b2461e19b00e74c.jpg",
+        title: "Toy",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "http://embed.cdn.pais.scholastic.com/v1/products/identifiers/isbn/9781338236132/primary/renditions/600?useMissingImage=true",
+        title: "Books",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiIyZqEsCNYUq79jZERlJQ0CQML6nRjW8PYyt5kgQIvpto9eFHAo-rsVOkkSwFxaV3i-g&usqp=CAU",
+        title: "Bikes",
+        description: "Toy trucks are fun to play alone but they are more fun when played with others. Playing with others is good for developing better social interactions",
+        price: 276016,
+    },
+])
 
 Review.create(product_id: 1, user_id: 2, name: "Ian", description: "Cool")
 
